d25cc9948e2e3d704005e9c01c1de9b0a288c1ba
steeeeee <stefano.giometti@gmail.com>
2018-04-27 17:02:31 +0200

    Adds iterm2 prefs and enables update check on repository

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Ask for the administrator password upfront
+
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Setups messages colors
@@ -27,8 +29,15 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Update dotfiles itself first
 
-# if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
 
 . "$DOTFILES_DIR/scripts/install-homebrew.sh"
 . "$DOTFILES_DIR/scripts/install-dotfiles.sh"
 
+
+# iterm2
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_DIR/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
